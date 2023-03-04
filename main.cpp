3565a816cebe7555a059cb77fb80456bc7b985e3
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-03-04 15:27:58 +0000

    ultrasonic_sample

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "hcsr04.h"
+Serial pc(USBTX, USBRX); 
+HCSR04  usensor(p21,p12);
+int dist;
+int main()
+{
+    while(1) {
+        usensor.start();
+        dist=usensor.get_dist_cm();
+        pc.printf("distance = %d [cm]\r\n",dist);
+        wait(1);
+    }
 }
